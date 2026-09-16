172b468750a3cdb8fbf72c7ff5591d14443442e3
Uditanshu Singh <umangsingh852@gmail.com>
2026-09-16 05:11:03 +0000

    some essential changes

--- index4.js
+++ index4.js
@@ -37,46 +37,68 @@ const User = mongoose.model('user', userSchema);
 //Middleware
 app.use(express.urlencoded({extended: false}));
 
+app.use((req, res, next) => {
+    fs.appendFile(
+        "logs.txt",
+        `\n${Date.now()}:${req.ip}:${req.method}:${req.path}\n`,
+        (err, data) => {
+            next();
+        }
+    );
+});
+
+
 app.get("/users", (req, res) => {
     const html = `
     <ul>
-        ${users.map(user => `<li>${user.first_name}</li>`).join("")}
+        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
     </ul>
     `;
-
     res.send(html);
 });
 
 // REST APIs
-
 app.get("/api/users", (req, res) => {
+    res.setHeader("X-MyName", "Uditanshu"); //Custom Header
     return res.json(users);
 });
 
-app.route("/api/users/:id")
+app
+    .route("/api/users/:id")
     .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
+            const id = Number(req.params.id);
+            const user = users.find((user) => user.id === id);
 
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
+            if (!user) {
+                return res.status(404).json({ message: "User not found" });
+            }
+            return res.json(user);
+        })
+        .patch((req, res) => {
+            return res.json({ status: "pending" });
+        })
+        .delete((req, res) => {
+            return res.json({ status: "pending" });
+        });
 
-        return res.json(user);
-    })
-    .patch((req, res) => {
-        return res.json({ status: "pending" });
-    })
-    .delete((req, res) => {
-        return res.json({ status: "pending" });
-    });
-
-app.post("/api/users", (req, res) => {
+app.post("/api/users", async(req, res) => {
     const body = req.body;
-    users.push({...body, id: users.length }); 
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-        return res.json({ status: "Success", id: users.length + 1});
+    if (!body.first_name ||
+        !body.last_name ||
+        !body.email ||
+        !body.gender ||
+        !body.job_title) {
+        return res.status(400).json({ message: "All Fields Required" });
+    }
+
+    await User.create({
+        first_name: body.first_name,
+        last_name: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        job_title: body.job_title
     });
+    return res.status(201).json({ message: "sucess" });
 });
 
 app.listen(PORT, () => {
